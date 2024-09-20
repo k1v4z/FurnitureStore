e694540465c0ec1fba52ae4ff7f9dea0c9908e39
k1v4z <addnguyen32gmail.com>
2024-09-20 15:24:01 +0700

    add header components and add ant design icon

---
+++ app/components/Header.tsx
@@ -0,0 +1,20 @@
+import { ShoppingCartOutlined } from '@ant-design/icons'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='bg-yellow-100 py-5'>
+        <div className='container mx-auto flex items-center justify-between'>
+          <nav>
+            <ul className='flex space-x-4'>
+              <li><a href="#" className='hover:underline cursor-pointer font-serif'>Home</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
